87654b596efc3bab62dcb500a0274d506180b6ea
dankogai <dankogai@d0d07461-0603-4401-acd4-de1884942a52>
2009-05-10 07:55:32 +0000

    URI::Amazon::APA - URI to access Amazon Product Advertising API
    git-svn-id: http://svn.coderepos.org/share/lang/perl/URI-Amazon-APA/trunk@33214 d0d07461-0603-4401-acd4-de1884942a52

---
+++ lib/URI/Amazon/APA.pm
@@ -0,0 +1,107 @@
+package URI::Amazon::APA;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+URI::Amazon::APA - The great new URI::Amazon::APA!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use URI::Amazon::APA;
+
+    my $foo = URI::Amazon::APA->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Dan Kogai, C<< <dankogai at dan.co.jp> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-uri-amazon-apa at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=URI-Amazon-APA>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc URI::Amazon::APA
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=URI-Amazon-APA>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/URI-Amazon-APA>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/URI-Amazon-APA>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/URI-Amazon-APA/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Dan Kogai, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of URI::Amazon::APA
